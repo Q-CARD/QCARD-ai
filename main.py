ba018a283e2774c14003c1f4bd1869391dbcaa25
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-30 01:21:37 +0900

    [FEAT] 데이터베이스 연결

--- main.py
+++ main.py
@@ -3,13 +3,29 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://localhost:3000"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/interview/start")
+async def start_interview():
+    return ""
+
+
+@app.post("/interview/answer")
+async def answer_interview():
+    return ""
+
+
+@app.get("/interview/{interview_id}")
+async def get_interview_result():
+    return ""
---
+++ model.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Date, DateTime, String, BigInteger
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Account(Base):
+    __tablename__ = "account"
+    id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    email = Column(String)
+    name = Column(String)
+
+
+class Question(Base):
+    __tablename__ = "question"
+    id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    title = Column(String)
+    category = Column(String)
+
+
+class Inteverview(Base):
+    id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    account = Column(BigInteger, ForeignKey("account.id"))
+    created_at = Column(DateTime)
+    category_1 = Column(String)
+    category_2 = Column(String)
+    category_3 = Column(String)
+    interview_account = relationship("Account", backref="interview_account")
+
+
+class InterviewQuestion(Base):
+    id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
+    interview = Column(BigInteger, ForeignKey("interview.id"))
+    question = Column(BigInteger, ForeignKey("question.id"))
+    answer = Column(String)
+    gpt_answer = Column(String)
+    additional_question_1 = Column(String)
+    additional_answer_1 = Column(String)
+    additional_question_2 = Column(String)
+    additional_answer_2 = Column(String)
+    additional_question_3 = Column(String)
+    additional_answer_3 = Column(String)
+    interview_model = relationship("Interview", backref="interview_model")
+    question_model = relationship("Question", backref="question_model")
