91b3b884304211dc7793011b09895cfdb3cceea2
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-08-31 20:10:15 +0900

    [FEAT] Whisper 호출&응답 성공

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # QCARD-ai
+
+### 실행 명령어
+```bash
+uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, Depends, Header
+import os
+import shutil
+
+from fastapi import FastAPI, Depends, Header, UploadFile
 from starlette.middleware.cors import CORSMiddleware
 import database
 from scheme import InterviewStartReq
 from sqlalchemy.orm import Session
-from util import jwt_util
+from util import jwt_util, gpt_util, whisper_util
 import crud
 
 app = FastAPI()
@@ -14,6 +17,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+UPLOAD_DIR = "/tmp"
 
 
 def get_db():
@@ -38,7 +43,8 @@ async def start_interview(req: InterviewStartReq, Authorization: str | None = He
         questions = crud.find_all_question(db=db)
     else:
         questions = crud.find_question_by_categories(db=db, categories=req.categories)
-    interview, interview_question = crud.create_interview(db=db, account=account, questions=questions, categories=req.categories)
+    interview, interview_question = crud.create_interview(db=db, account=account, questions=questions,
+                                                          categories=req.categories)
     return {
         "interview_id": interview.id,
         "account": {
@@ -50,9 +56,25 @@ async def start_interview(req: InterviewStartReq, Authorization: str | None = He
     }
 
 
-@app.post("/interview/answer")
-async def answer_interview():
-    return ""
+@app.put("/interview/answer/{question_id}")
+async def answer_interview(question_id: int, file: UploadFile, Authorization: str | None = Header(default=None),
+                           db: Session = Depends(get_db)):
+    # 계정 유효성 검증
+    account = crud.find_account_by_email(db=db, email=jwt_util.decode_jwt(access_token=Authorization))
+
+    # 파일 업로드
+    filename = file.filename
+    file_obj = file.file
+    upload_name = os.path.join(UPLOAD_DIR, filename)
+    upload_file = open(upload_name, 'wb+')
+    shutil.copyfileobj(file_obj, upload_file)
+    upload_file.close()
+
+    # Whisper-api 호출
+    answer = whisper_util.translate_answer_audio(file=upload_name)
+
+
+    return account
 
 
 @app.get("/interview/{interview_id}")
