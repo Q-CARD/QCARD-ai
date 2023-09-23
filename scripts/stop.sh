805c930df290cf49d9d43cc9c8f1d493b9711052
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-09-23 15:40:30 +0900

    [CHORE] 도커 이미지 삭제 시 강제 조건 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,5 +5,5 @@ echo "> Docker compose down"
 
 sudo docker-compose down
 
-sudo docker rm $(sudo docker ps -aq)
-sudo docker rmi $(sudo docker images -q)
+sudo docker rm -f $(sudo docker ps -aq)
+sudo docker rmi -f $(sudo docker images -q)
